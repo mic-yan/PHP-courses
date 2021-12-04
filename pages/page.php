208886d0851377a39cb4adf797c4eb31ff0077f1
Amasty Support <cloud-access@amasty.com>
2021-12-04 19:08:02 +0300

    финальная версия

---
+++ pages/page.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Страница <?=$page?></title>
+</head>
+<body>
+<h1>Страница <?=$page?></h1>
+<div>
+    <?php
+    $a = output_text ($page_text);
+    $a = one_word ('что', $a);
+    $a = bold_first_letter($a);
+    echo $a;
+    ?>
+</div>
+<div>
+    <?php
+    echo '<p>Кол-во предложений: '.text_sentences($page_text).'</p>
+          <p>Кол-во слов: '.text_words($page_text).'</p>
+          <p>Кол-во символов: '.text_symbols($page_text).'</p>';
+    ?>
+</div>
+<div>
+    <?php
+    echo links ($page, $pages_count, 'index.php', 'page');
+    ?>
+</div>
+</body>
+</html>
