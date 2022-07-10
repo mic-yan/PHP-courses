9e1cd428c8c932c2ea2acee71b67300f77e96545
num16th <michael.yanushkevich@gmail.com>
2022-07-10 19:07:39 +0300

    Update

--- files_date_time/1.php
+++ files_date_time/1.php
@@ -2,22 +2,23 @@
 
 $text = file_get_contents("text.txt"); // получаем содержимоей файла (текст)
 
-$sentences = preg_split('/(?<=[.?!])\s+(?=[a-z])/i', $text); // разделяем текст на предложения
+$sentences = preg_split('/(?<=[.?!])\s+(?=[a-z])/i', $text); // разделяем текст на предложения через массив
 
 print_r($sentences);
 
-$keynum = count($sentences);
-// echo $keynum;
-$array = array();
-for ($i = 0; $i < $keynum; $i++) {
-    if (stripos($sentences[$i], 'lorem')) {
+$keynum = count($sentences); // подсчитываем кол-во ключей
+
+$array = array(); // объявляем массив для предложений после выборки
+
+for ($i = 0; $i  < $keynum; $i++) { // выборка предложений по условию нескольких слов
+    if ((stripos($sentences[$i], "ipsum") && stripos($sentences[$i], "sit") && stripos($sentences[$i], "amet")) !== false) {
         $array[] = $sentences[$i];
     }
 }
 
-print_r($array);
+print_r($array); // смотрим результаты
 
-$backtotext = implode("\n", $array);
+$backtotext = implode("\n", $array); // трансформируем массив обратно в текст
 
-$newfile = fopen('new_text.txt', 'w');
-fwrite($newfile, $backtotext);
+$newfile = fopen('new_text.txt', 'w'); // создаем новый файд
+fwrite($newfile, $backtotext); // записываем в него текст
